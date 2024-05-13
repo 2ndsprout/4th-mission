36971aea290851b88cb2a9c89337b4ae8bb304e2
2ndsprout <ralphlauren5243@gmail.com>
2024-05-13 11:25:15 +0900

    keyword hold in text input

--- src/main/java/com/example/mission_/note/MainController.java
+++ src/main/java/com/example/mission_/note/MainController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.NoSuchElementException;
 
@@ -15,12 +16,13 @@ public class MainController {
 
 
     @GetMapping("/")
-    public String main(String keyword, String isSearchModal, Model model) {
+    public String main(String keyword, @RequestParam(defaultValue = "false") String isSearchModal, Model model) {
 
         try {
             MainDataDto mainDataDto = this.mainService.getDefaultMainData(keyword);
             model.addAttribute("mainDataDto", mainDataDto);
             model.addAttribute("isSearchModal", isSearchModal);
+            model.addAttribute("keyword", keyword);
         }
         catch (NoSuchElementException e) {
          e.printStackTrace();
